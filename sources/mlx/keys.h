5e13f2dd358b0fe4a7415efe813bff6e424bdaea
Alexandre Hallaine <ahallain@student.42.fr>
2020-10-18 23:39:28 +0200

    42 norm

--- sources/mlx/keys.h
+++ sources/mlx/keys.h
@@ -1,27 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   update_full.h                                      :+:      :+:    :+:   */
+/*   keys.h                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ahallain <ahallain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/10/18 23:32:58 by ahallain          #+#    #+#             */
-/*   Updated: 2020/10/18 23:32:59 by ahallain         ###   ########.fr       */
+/*   Created: 2020/10/18 23:32:50 by ahallain          #+#    #+#             */
+/*   Updated: 2020/10/18 23:35:04 by ahallain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef UPDATE_FULL_H
-# define UPDATE_FULL_H
+#ifndef KEYS_H
+# define KEYS_H
+# include "mlx1.h"
 
-# include "../mlx_full.h"
-
-typedef struct	s_sprite_variable
+typedef struct	s_key
 {
-	t_texture		texture;
-	t_position		transform;
-	t_resolution	sprite;
-}				t_sprite_variable;
+	unsigned short	input;
+	t_key_f			function;
+	char			index;
+	bool			one_time;
+}				t_key;
 
-void			sprite_loop_x(t_runtime *runtime, t_sprite_variable sprite,
-	t_resolution x_max, t_resolution y_max);
+void			w(t_runtime *runtime);
+void			s(t_runtime *runtime);
+void			a(t_runtime *runtime);
+void			d(t_runtime *runtime);
+void			left(t_runtime *runtime);
+void			right(t_runtime *runtime);
+void			f2(t_runtime *runtime);
+void			esc(t_runtime *runtime);
 #endif
--- sources/mlx/mlx_key.h
+++ sources/mlx/mlx_key.h
@@ -6,7 +6,7 @@
 /*   By: ahallain <ahallain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/14 22:05:55 by ahallain          #+#    #+#             */
-/*   Updated: 2020/10/16 19:39:39 by ahallain         ###   ########.fr       */
+/*   Updated: 2020/10/18 23:35:14 by ahallain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,38 +18,24 @@
 # include <X11/keysymdef.h>
 # include <stdbool.h>
 # include "mlx1.h"
-
-typedef struct	s_key
-{
-	unsigned short	input;
-	t_key_f			function;
-	char			index;
-	bool			one_time;
-}				t_key;
-
-void			w(t_runtime *runtime);
-void			s(t_runtime *runtime);
-void			a(t_runtime *runtime);
-void			d(t_runtime *runtime);
-void			left(t_runtime *runtime);
-void			right(t_runtime *runtime);
-void			f2(t_runtime *runtime);
-void			esc(t_runtime *runtime);
-
+# include "keys.h"
 
 # if BONUS
-	#  include "../../bonus/mlx/bonus_mlx_key.h"
-# else
-	t_key	g_key_board[] = {
-		{XK_w, &w, -1, false},
-		{XK_s, &s, -1, false},
-		{XK_a, &a, -1, false},
-		{XK_d, &d, -1, false},
-		{XK_Left, &left, -1, false},
-		{XK_Right, &right, -1, false},
-		{XK_F2, &f2, -1, false},
-		{XK_Escape, &esc, -1, false},
-		{0, 0, 0, 0}
-	};
+#  include "../../bonus/mlx/bonus_mlx_key.h"
+# endif
+
+# if !BONUS
+
+t_key	g_key_board[] = {
+	{XK_w, &w, -1, false},
+	{XK_s, &s, -1, false},
+	{XK_a, &a, -1, false},
+	{XK_d, &d, -1, false},
+	{XK_Left, &left, -1, false},
+	{XK_Right, &right, -1, false},
+	{XK_F2, &f2, -1, false},
+	{XK_Escape, &esc, -1, false},
+	{0, 0, 0, 0}
+};
 # endif
 #endif
