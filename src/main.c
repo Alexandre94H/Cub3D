4fff43833c18621f4cd43ebaf3cb135176cd16a1
Alexandre Hallaine <hallainealexandre@gmail.com>
2024-04-23 12:53:36 -0400

    Fix: block ratio

--- src/main.c
+++ src/main.c
@@ -27,15 +27,16 @@ int main(int argc, char **argv) {
 		.dir = { sin(g_data.file.player_angle * M_PI / 180), -cos(g_data.file.player_angle * M_PI / 180) },
 	};
 
-	float ratio = (float)g_data.file.resolution[0] / g_data.file.resolution[1];
-	g_data.player.plane[0] = -g_data.player.dir[1] / ratio;
-	g_data.player.plane[1] = g_data.player.dir[0] / ratio;
-
 	mlx_set_setting(MLX_STRETCH_IMAGE, true);
 	mlx_t *mlx = mlx_init(g_data.file.resolution[0], g_data.file.resolution[1], argv[0], true);
+
+	g_data.player.plane[0] = -0.5 * mlx->width / mlx->height * g_data.player.dir[1];
+	g_data.player.plane[1] = -0.5 * mlx->height / mlx->width * g_data.player.dir[0];
+
 	mlx_key_hook(mlx, key, mlx);
 	mlx_loop_hook(mlx, hook_generic, mlx);
 	mlx_loop_hook(mlx, loop, mlx);
+
 	mlx_loop(mlx);
 	mlx_terminate(mlx);
 	free(g_data.file.map);
